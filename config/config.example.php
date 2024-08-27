68aa23a892710107e2d631721248badad9c445a1
LGCTDEV <lgctdev>
2024-08-27 15:17:41 +0200

    Initial commit on dev branch with project structure and basic config for dev and tests

---
+++ config/config.example.php
@@ -0,0 +1,9 @@
+<?php
+// config/config.example.php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'your_user');        // Remplace par ton utilisateur MySQL
+define('DB_PASS', 'your_password');    // Remplace par ton mot de passe MySQL
+define('DB_NAME', 'your_database');    // Nom de la base de données
+
+// Autres configurations générales
